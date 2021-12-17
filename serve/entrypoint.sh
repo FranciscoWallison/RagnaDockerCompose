65b3eff32b7fe738e52e9e380b91f62e8d5e3b7a
Francisco Wallison <franciscowallison@gmail.com>
2021-12-17 08:24:36 -0300

    Update entrypoint.sh

--- serve/entrypoint.sh
+++ serve/entrypoint.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-sed -i 's/_ip: 127.0.0.1/_ip: db/g' ./conf/inter_athena.conf
-echo "Formart file" 
-dos2unix -f configure 
-dos2unix -f athena-start 
-dos2unix -f function.sh 
-echo "Giving permissions" 
-chmod a+x athena-start 
-chmod a+x configure 
-chmod a+x function.sh 
-echo "Stop the server if it is started" 
-./athena-start stop 
-echo "Run the configure script" 
-./configure 
- echo "Finally compile Emulator" 
-./configure --enable-packetver=20180131 make clean server 
- echo "Start server" 
-./athena-start start
+sed -i 's/_ip: 127.0.0.1/_ip: db/g' ./conf/inter_athena.conf &&
+echo "Formart file" &&
+dos2unix -f configure &&
+dos2unix -f athena-start &&
+dos2unix -f function.sh &&
+echo "Giving permissions" &&
+chmod a+x athena-start &&
+chmod a+x configure &&
+chmod a+x function.sh &&
+echo "Stop the server if it is started" &&
+./athena-start stop &&
+echo "Run the configure script" &&
+./configure &&
+ echo "Finally compile Emulator" &&
+ make clean server &&
+ echo "Start server" &&
+./athena-start start&&
 
 
 # sed -i 's/_ip: 127.0.0.1/_ip: db/g' ./conf/inter_athena.conf
