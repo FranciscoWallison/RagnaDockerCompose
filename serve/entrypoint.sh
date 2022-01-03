4624257674c72c584126216fdce892e7745dca86
Francisco Wallison <franciscowallison@gmail.com>
2022-01-03 10:40:36 -0300

    Update entrypoint.sh

--- serve/entrypoint.sh
+++ serve/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sed -i 's/_ip: 127.0.0.1/_ip: db/g' ./conf/inter_athena.conf &&
+ sed -i 's/_ip: 127.0.0.1/_ip: db/g' ./conf/inter_athena.conf &&
+ sed -i 's/\/\/login_ip: 127.0.0.1/login_ip: serve/g' ./conf/char_athena.conf &&
+ sed -i 's/\/\/char_ip: 127.0.0.1/char_ip: 127.0.0.1/g' ./conf/char_athena.conf &&
+ sed -i 's/\/\/char_ip: 127.0.0.1/char_ip: serve/g' ./conf/map_athena.conf &&
+ sed -i 's/\/\/map_ip: 127.0.0.1/map_ip: 127.0.0.1/g' ./conf/map_athena.conf &&
 echo "Formart file" &&
 dos2unix -f configure &&
 dos2unix -f athena-start &&
@@ -17,12 +21,6 @@ echo "Run the configure script" &&
  make clean server &&
  echo "Start server" &&
 ./athena-start start
-
-
-sed -i 's/\/\/login_ip: 127.0.0.1/login_ip: serve/g' ./conf/char_athena.conf &&
-sed -i 's/\/\/char_ip: 127.0.0.1/char_ip: 127.0.0.1/g' ./conf/char_athena.conf &&
-sed -i 's/\/\/char_ip: 127.0.0.1/char_ip: serve/g' ./conf/map_athena.conf &&
-sed -i 's/\/\/map_ip: 127.0.0.1/map_ip: 127.0.0.1/g' ./conf/map_athena.conf 
 
 echo "SUCESSOOOOOOOO !!!!!!!!!!!!!"
 while [ 1 ]; do
