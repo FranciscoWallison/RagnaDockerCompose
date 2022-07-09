275d62bc564545fbe8cec961438661cf471a109d
Francisco Wallison <franciscowallison@gmail.com>
2022-07-09 09:44:55 -0300

    config create serve

--- serve/entrypoint.sh
+++ serve/entrypoint.sh
@@ -1,31 +1,4 @@
 #!/bin/bash
-
-<<<<<<< HEAD
-sed -i 's/_ip: 127.0.0.1/_ip: db/g' ./conf/inter_athena.conf
-echo "Formart file" 
-dos2unix -f configure 
-dos2unix -f athena-start 
-dos2unix -f function.sh 
-echo "Giving permissions" 
-chmod a+x athena-start 
-chmod a+x configure 
-chmod a+x function.sh 
-echo "Stop the server if it is started" 
-./athena-start stop 
-echo "Run the configure script" 
-./configure 
- echo "Finally compile Emulator" 
-./configure --enable-packetver=20200401 && make clean server 
- echo "Start server" 
-./athena-start start
-
-
-# sed -i 's/login_ip: serve///login_ip: 127.0.0.1/g' ./conf/char_athena.conf
-# sed -i 's/char_ip: 127.0.0.1///char_ip: 127.0.0.1/g' ./conf/char_athena.conf
-# sed -i 's/char_ip: serve///char_ip: 127.0.0.1/g' ./conf/inter_athena.conf
-# sed -i 's/map_ip: 127.0.0.1///map_ip: 127.0.0.1/g' ./conf/inter_athena.conf
-
-=======
  sed -i 's/_ip: 127.0.0.1/_ip: db/g' ./conf/inter_athena.conf &&
  sed -i 's/\/\/login_ip: 127.0.0.1/login_ip: serve/g' ./conf/char_athena.conf &&
  sed -i 's/\/\/char_ip: 127.0.0.1/char_ip: 127.0.0.1/g' ./conf/char_athena.conf &&
@@ -43,12 +16,11 @@ echo "Stop the server if it is started" &&
 ./athena-start stop &&
 echo "Run the configure script" &&
 ./configure &&
+# ./configure --enable-packetver=20200401 && make clean server 
  echo "Finally compile Emulator" &&
  make clean server &&
  echo "Start server" &&
 ./athena-start start
-
->>>>>>> 56cdc98bcfa1e35035c14a3e6704628f58d5a212
 echo "SUCESSOOOOOOOO !!!!!!!!!!!!!"
 while [ 1 ]; do
     /bin/bash
